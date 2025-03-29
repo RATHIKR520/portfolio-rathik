44e30712e7c36dcbc41f129f6b054f38c99a8ce4
RATHIKR520 <rathikpoojary9686@gmail.com>
2025-03-29 18:47:38 +0530

    rebuild

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,4 @@
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'; // 1. Import BrowserRout
 import App from './App.tsx'
 import './index.css'
-
-
-createRoot(document.getElementById("root")!).render(
-    // 2. Wrap App with BrowserRouter
-    <BrowserRouter basename="/automated-journey-narrative">
-      <App />
-    </BrowserRouter>
-  );
+createRoot(document.getElementById("root")!).render(<App />);
